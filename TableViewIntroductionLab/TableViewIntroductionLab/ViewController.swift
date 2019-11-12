9c1665d50a3a8519044ea0ed34a644182ff73e6d
oscar victoria <oscarvictoriagonzalez@gmail.com>
2019-11-12 17:31:23 -0500

    completed part 1

--- TableViewIntroductionLab/TableViewIntroductionLab/ViewController.swift
+++ TableViewIntroductionLab/TableViewIntroductionLab/ViewController.swift
@@ -9,12 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    private var task = Task.allTasks { // empty array
+           
+           didSet {
+               tableView.reloadData()
+           }
+           
+           
+       }
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
     }
-
-
+    
 }
 
+extension ViewController: UITableViewDataSource {
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return task.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "taskCell", for: indexPath)
+        
+        let tasks = task[indexPath.row]
+        
+        cell.textLabel?.text = tasks.name
+        cell.detailTextLabel?.text = tasks.dueDate.description
+        
+        return cell 
+    }
+}
